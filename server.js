4962e5552452d8147d5f12ba14e70c28ab950a9e
Klordok <kelsey@kelseykrehbiel.com>
2021-04-28 18:30:26 -0700

    working

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 console.log('hello')
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use((req, res) => {
+  res.send('\'ello, world!')
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
